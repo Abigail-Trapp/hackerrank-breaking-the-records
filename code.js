d6d6cd47024548bdfefa9d1df6ea43e381c7b900
Abigail Trapp <abbsthepandahead@gmail.com>
2022-01-25 22:52:52 -0700

    JavaScript commit

---
+++ code.js
@@ -0,0 +1,20 @@
+function breakingRecords(scores) {
+    let most = scores[0]
+    let least = scores[0]
+    let countmax = 0
+    let countmin = 0
+  
+    for (let i of scores){
+      if(i > most){
+        most = i
+        countmax += 1
+      }
+      if( i < least){
+        least = i
+        countmin += 1
+      }
+    }
+    return [countmax,countmin]
+  }
+  
+  console.log(breakingRecords([3,4,21,36,10,28,35,5,24,42]))
